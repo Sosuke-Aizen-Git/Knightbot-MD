f886ae827a02ef52e22335646923a6eea6fcf809
Sosuke-Aizen-Git <shosuke.aizen99@gmail.com>
2025-05-14 18:19:26 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -2,8 +2,8 @@ const settings = {
   packname: 'Knight Bot',
   author: 'Bot‎',
   botName: "WP bot",
-  botOwner: 'IntrovertSams', // Your name
-  ownerNumber: '918709298067', //Set your number here without + symbol, just add country code & number without any space
+  botOwner: 'IntrovertSama', // Your name
+  ownerNumber: '919229723625', //Set your number here without + symbol, just add country code & number without any space
   giphyApiKey: 'qnl7ssQChTdPjsKta2Ax2LMaGXz303tq',
   commandMode: "public",
   description: "This is a bot for managing group commands and automating tasks.",
